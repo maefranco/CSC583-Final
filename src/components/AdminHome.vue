23f504b08de4924d14552e46f0d331fc031ff6e8
Mae Franco <mfranco54@toromail.csudh.edu>
2017-12-12 19:53:06 -0800

    Updated AdminHome

---
+++ src/components/AdminHome.vue
@@ -0,0 +1,22 @@
+<template>
+  <table>
+    <tr>
+      <th>Name</th>
+      <th>Email</th>
+      <th>Phone</th>
+      <th>Company</th>
+      <th>Official Visit</th>
+      <th>Escort Required</th>
+      <th>Escort Name</th>
+    </tr>
+    <tr v-for="visitor in visitors">
+      <td>{{ visitor.name }}</td>
+      <td>{{ visitor.email }}</td>
+      <td>{{ visitor.phone }}</td>
+      <td>{{ visitor.company }}</td>
+      <td>{{ visitor.officialVisit }}</td>
+      <td>{{ visitor.escortRequired }}</td>
+      <td>{{ visitor.escortName }}</td>
+    </tr>
+  </table>
+</template>
